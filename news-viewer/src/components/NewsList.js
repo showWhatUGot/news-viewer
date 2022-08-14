34a949ce3d8d008138bb8ce2e510f3bda8a7e1c9
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-08-15 06:37:37 +0900

    api를 위한 컴포넌트 제작중

---
+++ news-viewer/src/components/NewsList.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+
+import React from 'react';
+
+const NewListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const sampleArticle = {
+  title: '제목',
+  description: '내용',
+  url: 'https://naver.com',
+  urlToImage: 'https://via.placeholder.com/160',
+};
+
+const NewsList = () => {
+  return (
+    <NewListBlock>
+      <NewsItem article={sampleArticle} />
+      <NewsItem article={sampleArticle} />
+      <NewsItem article={sampleArticle} />
+      <NewsItem article={sampleArticle} />
+      <NewsItem article={sampleArticle} />
+      <NewsItem article={sampleArticle} />
+      <NewsItem article={sampleArticle} />
+    </NewListBlock>
+  );
+};
+
+export default NewsList;
